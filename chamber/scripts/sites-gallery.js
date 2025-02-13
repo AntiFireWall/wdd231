b425439735871a7a0017c4f4605d3cdaddb9d094
BuildTools <unconfigured@null.spigotmc.org>
2025-02-13 12:44:19 +0400

    chamber Discover page

--- chamber/scripts/sites-gallery.js
+++ chamber/scripts/sites-gallery.js
@@ -1,6 +1,8 @@
 import {sites} from "../data/sites.mjs"
 const siteGallery = document.querySelector(".sites-gallery");
-console.log(siteGallery)
+const message = document.querySelector("#visit");
+const lastDate = localStorage.getItem("lastDate");
+console.log(lastDate)
 
 sites.forEach(site => {
     const name = document.createElement("h2");
@@ -16,6 +18,7 @@ sites.forEach(site => {
     address.textContent = site.address;
     description.textContent = site.description;
     image.src = site.image_url;
+    image.alt = site.name;
     imageFig.appendChild(image);
     learnMore.textContent = `Learn More`
     
@@ -27,3 +30,26 @@ sites.forEach(site => {
 
     siteGallery.appendChild(siteCard);
 });
+
+function setLastDate() 
+{
+    localStorage.setItem("lastDate", new Date())
+}
+
+function setMessage()
+{
+    if(lastDate == null)
+    {
+        message.textContent = `Welcome! Let us know if you have any questions.`;
+    } else if(((new Date().getHours()) - (new Date(lastDate).getHours())) <= 24) 
+    {
+        message.textContent = `Back so soon! Awesome!`;
+    } else
+    {
+        let days = (new Date().getDate()) - (new Date(lastDate).getDate())
+        message.textContent = `You last visited ${days != 1 ? `${days} days` : `${days} day`} ago.`; 
+    }
+}
+
+setLastDate();
+setMessage();
