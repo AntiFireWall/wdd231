16fedf81061fd14f0129b20250f6291b8bd74bc4
TheeJStar <kingoflightj@gmail.com>
2025-02-20 05:55:59 -0800

    added report bug page

--- project/scripts/getdates.js
+++ project/scripts/getdates.js
@@ -1,7 +1,11 @@
 const currentyear = document.querySelector('#currentyear');
 const lastModified = document.querySelector('#lastModified');
+const hiddenInput = document.querySelector('#hidden');
 const lastModifiedDate = new Date(document.lastModified);
 const today = new Date();
+if (hiddenInput) {
+    hiddenInput.value = Date()
+}
 
 currentyear.innerHTML = today.getFullYear()
 lastModified.innerHTML = `Last Modified: ${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
