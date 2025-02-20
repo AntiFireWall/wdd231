5c8de0cbe1cff50153389554be78cc978102663e
BuildTools <unconfigured@null.spigotmc.org>
2025-02-20 22:32:15 +0400

    report form look fix & thank you page report text formatting fix

--- project/scripts/thankyou.js
+++ project/scripts/thankyou.js
@@ -8,8 +8,14 @@ const date = document.createElement("p");
 
 displayName.innerHTML = `${data[0].split("=")[1]} ${data[1].split("=")[1]}`;
 email.innerHTML = `We will send you an update to <b>${data[2].split("=")[1]}</b>`;
-report.innerHTML = `This is what you wrote: <b>${data[3].split("=")[1]}</b>`;
-date.innerHTML = `at: ${data[4].split("=")[1]}`;
+report.innerHTML = `This is what you wrote: <b>${data[3].split("=")[1]
+    .replaceAll("%27", "'")
+    .replaceAll("%2B", "+")
+    .replaceAll("%40", "@")
+    .replaceAll("%28", "(")
+    .replaceAll("%29", ")")
+    .replaceAll("%3A", "-")}</b>`;
+date.innerHTML = `at: ${data[4].split("=")[1].replaceAll("%2B", "+")}`;
 
 infobox.appendChild(displayName);
 infobox.appendChild(email);
